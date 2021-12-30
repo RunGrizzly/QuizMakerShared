e75802b68c64d1c1175ad57aa8bf4c3752e4a81d
Josh Desktop | P400A <joshua.henderson006@gmail.com>
2021-12-30 23:16:18 +0000

    new commit

---
+++ Code/SpawnGameObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGameObject : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject gameObject = new GameObject("Test gameObject");
+    }
+}
